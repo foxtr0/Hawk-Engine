a69bbf58d6fe078974206d8b12dc9fbd4cf1c08f
foxtro <93345005+dakoolfox@users.noreply.github.com>
2022-01-16 21:01:09 +0530

    Update tile.py

--- HawkEngine/src/tile.py
+++ HawkEngine/src/tile.py
@@ -4,6 +4,7 @@ class Tile:
     def __init__(
         self,
         image:pygame.Surface,
+        type,
         x,
         y,
         window
@@ -12,7 +13,9 @@ class Tile:
         self.rect=self.image.get_rect()
         self.rect.x,self.rect.y=x,y
         self.window=window
+        self.type=type
     def draw(
         self
         ):
+        pygame.draw.rect(self.window,(30,30,30),self.rect)
         self.window.blit(self.image,(self.rect.x,self.rect.y))
