49ea11d64bafaf4c51a30efdb34c93235ae04e54
foxtro <93345005+dakoolfox@users.noreply.github.com>
2022-01-16 20:59:58 +0530

    Update entity.py

--- HawkEngine/src/entity.py
+++ HawkEngine/src/entity.py
@@ -1,6 +1,5 @@
 import pygame
-
-from src.controller import Controller
+from controller import Controller
 
 class Entity:
     def __init__(
@@ -10,33 +9,56 @@ class Entity:
         window:pygame.Surface=None,
         health:float=100,
         position:list=[0,0],
-        controller:Controller=None
+        controller:Controller=None,
+        amount:int=1
         ):
         self.window=window
         self.sprite=sprite
-        self._rect=pygame.Rect(0,0,0,0)
         self.tag=tag
         self.health=health
         self.position=position
-    def rect(
-        self
-        )->pygame.Rect:
-        if self.sprite is not None:
-            w,h=self.sprite.get_size()
-            self._rect.width=w
-            self._rect.height=h
-            self._rect.y=self.position[1]
-            self._rect.x=self.position[0]
-        return self._rect
+        self.rect=pygame.Rect(self.position[0],self.position[1],self.sprite.get_width(),self.sprite.get_height())
+        self.killed=False
+        self.controller=controller
     def render(
         self
         )->None:
-        self.window.blit(self.sprite,self._rect)
+        self.window.blit(self.sprite,self.rect)
+    def collide(
+        self,
+        tile_list
+        ):
+        collision_types={"right":False,"left":False,"top":False,"bottom":False}
+        self.rect.x+=self.position[0]
+        hit=[]
+        for tile in tile_list:
+            if self.rect.colliderect(tile.rect):
+                hit.append(tile)
+        for tile in hit:
+            if self.position[0]>0:
+                self.rect.right=tile.rect.left
+                collision_types["right"]=True
+            if self.position[0]<0:
+                self.rect.left=tile.rect.right
+                collision_types["left"]=True
+        self.rect.y+=self.position[1]
+        hit=[]
+        for tile in tile_list:
+            if self.rect.colliderect(tile.rect):
+                hit.append(tile)
+        for tile in hit:
+            if self.position[1]>0:
+                self.rect.bottom=tile.rect.top
+                collision_types["bottom"]=True
+            if self.position[1]<0:
+                self.rect.top=tile.rect.bottom
+                collision_types["top"]=True
+        return collision_types
+        
     def debug(
         self
         )->None:
-        #self.position[0] += 4
-        pygame.draw.rect(self.window,(255,255,255),self._rect,1)
+        pygame.draw.rect(self.window,(30,25,255),self.rect,1)
         
             
         
