c51996ee80efb2f959eedc97ebe9fa5f18247b38
foxtro <93345005+dakoolfox@users.noreply.github.com>
2022-01-21 17:45:03 +0530

    Add files via upload

---
+++ HawkEngine/src/scene.py
@@ -0,0 +1,21 @@
+import pygame
+
+
+
+class Scene:
+    def __init__(
+        self,
+        window,
+        clock,
+        fps
+        ):
+        self.window=window
+        self.clock=clock
+        self.dt=self.clock.tick(fps)
+        self.running=False
+        self.paused=False
+    def update(
+        self
+        ):
+        """Update function"""
+        pass
