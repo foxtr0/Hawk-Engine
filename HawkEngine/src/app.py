07123566b1a5e89298cd49e426e05a1deb480ab7
foxtro <93345005+dakoolfox@users.noreply.github.com>
2022-01-24 13:24:32 +0530

    Add files via upload

---
+++ HawkEngine/src/app.py
@@ -0,0 +1,27 @@
+import pygame
+import sys
+
+class App:
+    def __init__(
+        self,
+        title,
+        size,
+        fps
+        ):
+        self.window=pygame.display.set_mode(size)
+        self.clock=pygame.time.Clock()
+        self.fps=fps
+        self.title=pygame.display.set_caption(title)
+    def quit(
+        self
+        ):
+        sys.exit()
+    def start(
+        self
+        ):
+        for event in pygame.event.get():
+            if event.type==pygame.QUIT:
+                sys.exit()
+        self.window.fill((0,0,0))    
+        pygame.display.update()
+        self.clock.tick(60)
