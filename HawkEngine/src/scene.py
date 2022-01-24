80f3a36326765ccf7d54969daf1d25e3e5439066
foxtro <93345005+dakoolfox@users.noreply.github.com>
2022-01-24 13:27:16 +0530

    Update scene.py

--- HawkEngine/src/scene.py
+++ HawkEngine/src/scene.py
@@ -1,18 +1,25 @@
 import pygame
 
-
-
 class Scene:
     def __init__(
         self,
         window,
-        clock
+        clock,
+        fps
         ):
         self.window=window
         self.clock=clock
+        self.dt=self.clock.tick(fps)
         self.running=False
         self.paused=False
-    def start(
+    def pause(
+        self
+        )->bool:
+        self.paused=True
+        return self.paused
+    def update(
         self
         ):
+        """Update function"""
         pass
+        
