547101e760378d1a4e194597997c03a8d171273a
foxtro <93345005+dakoolfox@users.noreply.github.com>
2022-02-01 12:32:16 +0530

    Update tile.py

--- HawkEngine/src/tile.py
+++ HawkEngine/src/tile.py
@@ -7,15 +7,19 @@ class Tile:
         type,
         x,
         y,
-        window
+        window,
+        camera
         ):
         self.image=image
         self.rect=self.image.get_rect()
         self.rect.x,self.rect.y=x,y
         self.window=window
         self.type=type
+        self.camera=camera
     def draw(
         self
         ):
-        pygame.draw.rect(self.window,(30,30,30),self.rect)
-        self.window.blit(self.image,(self.rect.x,self.rect.y))
+        if self.camera is None:
+            self.window.blit(self.image,(self.rect.x,self.rect.y))
+        if self.camera is not None:
+            self.window.blit(self.image, (self.rect.x-self.camera.scroll[0],self.rect.y-self.camera.scroll[1]))
