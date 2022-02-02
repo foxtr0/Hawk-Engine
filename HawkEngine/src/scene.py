a757f42015d43b0df1dbb945034932fbb2b6a182
foxtro <93345005+dakoolfox@users.noreply.github.com>
2022-02-02 11:12:32 +0530

    Update scene.py

--- HawkEngine/src/scene.py
+++ HawkEngine/src/scene.py
@@ -4,12 +4,10 @@ class Scene:
     def __init__(
         self,
         window,
-        clock,
-        fps
+        clock
         ):
         self.window=window
         self.clock=clock
-        self.dt=self.clock.tick(fps)
         self.running=False
         self.paused=False
     def pause(
