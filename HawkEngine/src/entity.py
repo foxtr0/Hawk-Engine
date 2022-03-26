0d704b533992daf98aa95daee8e16e0bdde9c4ba
foxtro <93345005+foxtr0@users.noreply.github.com>
2022-03-26 16:44:46 +0530

    Changed the position method from 'list' to 'Vector2'

--- HawkEngine/src/entity.py
+++ HawkEngine/src/entity.py
@@ -8,7 +8,7 @@ class Entity:
         sprite:pygame.Surface=None,
         window:pygame.Surface=None,
         health:float=100,
-        position:list=[0,0],
+        position:pygame.math.Vector2=pygame.math.Vector2(0,0),
         controller:Controller=None,
         amount:int=1,
         camera=None
@@ -18,7 +18,7 @@ class Entity:
         self.tag=tag
         self.health=health
         self.position=position
-        self.rect=pygame.Rect(self.position[0],self.position[1],self.sprite.get_width(),self.sprite.get_height())
+        self.rect=pygame.Rect(self.position.x,self.position.y,self.sprite.get_width(),self.sprite.get_height())
         self.killed=False
         self.alive=True
         self.controller=controller
@@ -52,28 +52,28 @@ class Entity:
         ):
         """Makes the entity collidable with the `Tile` class's rect"""
         collision_types={"right":False,"left":False,"top":False,"bottom":False}
-        self.rect.x+=self.position[0]
+        self.rect.x+=self.position.x
         hit=[]
         for tile in tile_list:
             if self.rect.colliderect(tile.rect):
                 hit.append(tile)
         for tile in hit:
-            if self.position[0]>0:
+            if self.position.x>0:
                 self.rect.right=tile.rect.left
                 collision_types["right"]=True
-            if self.position[0]<0:
+            if self.position.x<0:
                 self.rect.left=tile.rect.right
                 collision_types["left"]=True
-        self.rect.y+=self.position[1]
+        self.rect.y+=self.position.y
         hit=[]
         for tile in tile_list:
             if self.rect.colliderect(tile.rect):
                 hit.append(tile)
         for tile in hit:
-            if self.position[1]>0:
+            if self.position.y>0:
                 self.rect.bottom=tile.rect.top
                 collision_types["bottom"]=True
-            if self.position[1]<0:
+            if self.position.y<0:
                 self.rect.top=tile.rect.bottom
                 collision_types["top"]=True
         return collision_types
@@ -86,4 +86,3 @@ class Entity:
         )->None:
         """debugs the entity by drawing the rect"""
         pygame.draw.rect(self.window,(30,25,255),self.rect,1)
-
